9fb721ef7eb2ff372a895a953d3f46fdc098d7f2
liguanjian@rd.com <liguanjian@rd.com>
2017-01-13 12:37:03 +0800

    添加统计代码

---
+++ js/dc.js
@@ -0,0 +1,11 @@
+/**
+ * Created by liguanjian on 2017-1-13.
+ */
+
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?21caf162625a2b2e32750a368fb9dc56";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
